6fb8723b3a90a13e9f2283495495685aacac2f37
john michael <johnmickey281@gmail.com>
2023-08-06 19:53:54 -0500

    lets have swagger even in deployed app

--- Medfast.Services.MedicationAPI/Program.cs
+++ Medfast.Services.MedicationAPI/Program.cs
@@ -59,11 +59,9 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseCors("CorsPolicy");
 
