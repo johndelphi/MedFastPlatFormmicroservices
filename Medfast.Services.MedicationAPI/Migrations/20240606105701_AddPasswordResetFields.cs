14879453b44ea0c71740adcb7a6c4b699fe2feca
johnmichael <johnmickey281@gmail.com>
2024-06-06 05:58:44 -0500

    migrationforps

---
+++ Medfast.Services.MedicationAPI/Migrations/20240606105701_AddPasswordResetFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Medfast.Services.MedicationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpiration",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpiration",
+                table: "AspNetUsers");
+        }
+    }
+}
